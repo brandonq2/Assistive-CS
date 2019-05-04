bb9a71cd72341ccd34e6dc760acd76dfc79a9d42
Ahmad Al-Taha <sonoplex@utmuser164-128.wireless.utoronto.ca>
2019-05-04 17:05:53 -0400

    ui

---
+++ AndroidStdioProject/app/src/main/java/com/example/teamfinesse/HelpeeScreen.java
@@ -0,0 +1,20 @@
+package com.example.teamfinesse;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+class HelpeeScreen extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.helpee_main);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
--- AndroidStdioProject/app/src/main/java/com/example/teamfinesse/MainActivity.java
+++ AndroidStdioProject/app/src/main/java/com/example/teamfinesse/MainActivity.java
@@ -1,13 +1,41 @@
 package com.example.teamfinesse;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnHelpee;
+    Button btnHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnHelpee = (Button) findViewById(R.id.btnHelpee);
+        btnHelpee.setOnClickListener(this);
+        btnHelper = (Button) findViewById(R.id.btnHelper);
+        btnHelpee.setOnClickListener(this);
+
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+
+            case R.id.btnHelpee:
+                startActivity(new Intent(MainActivity.this, HelpeeScreen.class));
+                break;
+
+            case R.id.btnHelper:
+                //code
+                break;
+        }
     }
 }
