591c5dbb7c6f8cfdcd397e97add313b3734c4b8d
Shahmeer Shahid <shahmeer.shahid@mail.utoronto.ca>
2019-05-04 15:20:52 -0400

    2

---
+++ app/src/main/java/com/example/teamfinesse/HelpeeScreen.kt
@@ -0,0 +1,9 @@
+package com.example.teamfinesse
+
+import android.os.Parcel
+import android.os.Parcelable
+import android.support.v7.app.AppCompatActivity
+
+class HelpeeScreen() : AppCompatActivity(), Parcelable {
+    
+}
